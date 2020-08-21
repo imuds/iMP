c4fe0a7a6034cb3f0bd5e3686988cd0581c30d82
kanzakiy <kanzakiy@ucr.edu>
2020-08-21 08:11:03 -0700

    new version of script to make input files and adding readme

--- input/make_input.py
+++ input/make_input.py
@@ -4,7 +4,26 @@ import os
 """
 attempt to create input file for imp: ver.2
 """
+# use default input?
+# if yes, choose simulation mode
+# if dissolution is induced, a depth value is asked to which water depth is changed during event
+# if no, ask about backgraound value
+# and then the specified values
+# finally plot time evolutions 
+
 #--- List of input parameters
+str_tmp = raw_input('Use default backgroud values for input parameters [y/n] ?:\t')
+if str_tmp == 'y': default_ref = True
+else: default_ref = False
+str_tmp = raw_input('Use default temporal changes of input parameter values [y/n] ?:\t')
+if str_tmp == 'y': 
+    default_event = True
+    simmode = raw_input('What kind of simulation ["biot", "diss", "size" or "iso"] ?: \t')
+else: default_event = False
+str_tmp = raw_input('Plot temporal changes of input parameter values [y/n] ?:\t')
+if str_tmp == 'y': plt_timeline = True
+else: plt_timeline = False
+
 par_names = [
     'time'                  # time in yr 
     ,'temp'                 # temperature in Celcius 
@@ -30,14 +49,39 @@ n_par = len(par_names)
 dt = 10.
 #--- start and end time of simulation 
 time_start = 0.
-# time_end = 100e3   # choose for biot exp 
-time_end = 200e3   # choose for diss exp 
+if default_event:
+    if simmode == 'biot': 
+        time_end = 100e3   # choose for biot exp 
+        event_start = 40e3
+        event_end = 45e3
+    else : 
+        time_end = 200e3   # choose for diss exp 
+        event_start = 40e3
+        event_end = 90e3
+else:
+    # default values
+    time_end = 100e3   
+    event_start = 40e3
+    event_end = 45e3
+    print 
+    print '+++ simulation and event duration +++' 
+    print 'Default time of end of simulation, start of event and end of event are respecitvely:\n','{:.2e} {:.2e} {:.2e}'.format(time_end, event_start, event_end)
+    print '+++++++++++++++++++++++++++++++++++++'
+    strlist = raw_input(
+        'Enter the above three time values in single line \n'
+        + '(e.g., enter "200e3 50e3 100e3"):\n\t'
+        )  
+    time_values = [np.float64(i) for i in strlist.split()]
+    time_end, event_start, event_end = time_values
+    print 
+    
+    
 n_step = int((time_end-time_start)/dt) + 1
 
 inputdata = np.zeros((n_step,n_par),dtype=np.float)
 inputdata[:,par_names.index('time')]=np.linspace(time_start,time_end,n_step)
 #--- Base parameter values which are used unless specified (see below)
-par_values_base = [
+val_ref = [
     0.
     ,2.
     ,35.
@@ -45,13 +89,10 @@ par_values_base = [
     ,2211.
     ,2285.
     ,165.  #O2
-    # ,165.*0.01  #O2
     ,12e-6  
     ,12e-6*0.7  #OM
-    # ,12e-6*1.5 #OM 
     ,12e-6*1./9.*100.  # detrital 
-    ,0.5  # fraction fine 
-    # ,2e-6  # AOM 
+    ,0.5  # fraction fine
     ,0.0  # AOM 
     ,10.   # bottom of SRZ 
     ,0.    
@@ -60,45 +101,69 @@ par_values_base = [
     ,0.   
     ,dt
     ]
+if not default_ref:
+    print '=== background parameter values  ===' 
+    print 'parameters you can change:\n'+' '.join(par_names)
+    print 
+    print 'the reference backgound values of the above parameters are :\n', val_ref
+    print '===================================='
+    strlist = raw_input(
+        'Enter parameters which you want to change from the reference background values during events \n'
+        + 'choose from the above paramter list and put in single line (e.g.,  enter "d13c d18o capd47";\n'
+        + 'if you press enter without any input default values are assumed):\n\t'
+        )  
+    for par_name in strlist.split():
+        print 
+        par_ref_tmp = raw_input( 
+            'Specify '+par_name+' background value '
+            + '(e.g., enter "1.5"):\t'
+            )
+        val_ref[par_names.index(par_name)] = np.float64(par_ref_tmp)
+par_values_base = val_ref
 #--- specify parameter values when they are changed with time
 #      Ex) if you want a given parameter to pass points 1 at year 5 and 4 at year 10, 
 #      put a set of parameter-value and time lists [5,10],[1,4] 
-par_values_spec = [
-    []   # time
-    ,[]  # temp
-    ,[]  # sal
-    # ,[[time_start,40e3,45e3,85e3,90e3,time_end],[3.5,3.5,4.5,4.5,3.5,3.5]]  #dep 
-    # ,[[time_start,40e3,45e3,85e3,90e3,time_end],[3.5,3.5,5.0,5.0,3.5,3.5]]  #dep 
-    # ,[[time_start,time_end],[0.2,0.2]]  #dep 
-    # ,[[time_start,time_end],[0.5,0.5]]  #dep 
-    # ,[[time_start,time_end],[1.0,1.0]]  #dep 
-    # ,[[time_start,time_end],[2.0,2.0]]  #dep 
-    # ,[[time_start,time_end],[3.0,3.0]]  #dep 
-    ,[[time_start,time_end],[3.5,3.5]]  #dep 
-    # ,[[time_start,time_end],[4.5,4.5]]  #dep 
-    # ,[[time_start,time_end],[4.6,4.6]]  #dep 
-    # ,[[time_start,time_end],[5.0,5.0]]  #dep 
-    ,[]  # dic
-    ,[]  # alk
-    ,[]  # o2
-    ,[]  # ccflx
-    ,[]  # omflx 
-    ,[]  # detflx
-    ,[[time_start,40e3,65e3,90e3,time_end],[0.5,0.5,0.1,0.5,0.5]]  # flxfin
-    # ,[]  # flxfin
-    ,[]  # AOM
-    ,[]  # bottom of SRZ
-    ,[[time_start,40e3,45e3,85e3,90e3,time_end],[2.,2.,-1.,-1.,2.,2.]]  # d13c  (diss exp)
-    # ,[[time_start,40e3,45e3,time_end],[2.,2.,-1.,-1.]]  # d13c, choose for biot exp
-    ,[[time_start,40e3,65e3,90e3,time_end],[1.,1.,-1.,1.,1.]]  # d18o (diss exp)
-    # ,[[time_start,40e3,42.5e3,45e3,time_end],[1.,1.,-1.,1.,1.]]  # d18o  (biot exp)
-    ,[[time_start,40e3,65e3,90e3,time_end],[0.6,0.6,0.5,0.6,0.6]]  # capd47  
-    # ,[]  # capd47
-    ,[]  # 14cage
-    # ,[[time_start,time_end],[1e3,1e3]]  #14cage
-    # ,[[time_start,40e3,45e3,85e3,90e3,time_end],[10e3,10e3,5e3,5e3,10e3,10e3]]  # 14cage
-    ,[]  # dt
+event_list = [
+    [[time_start,time_end],[par_values_base[i],par_values_base[i]]] for i in range(n_par)
     ]
+if default_event:
+    if simmode == 'biot':
+        event_list[par_names.index('d13c')] = [[time_start,event_start,event_end,time_end],[2.,2.,-1.,-1.]]
+        event_list[par_names.index('d18o')] = [[time_start,event_start,0.5*(event_start+event_end),event_end,time_end],[1.,1.,-1.,1.,1.]]
+    else:
+        event_list[par_names.index('d13c')] = [[time_start,40e3,45e3,85e3,90e3,time_end],[2.,2.,-1.,-1.,2.,2.]] 
+        event_list[par_names.index('d18o')] = [[time_start,event_start,0.5*(event_start+event_end),event_end,time_end],[1.,1.,-1.,1.,1.]]
+        event_list[par_names.index('capd47')] = [[time_start,event_start,0.5*(event_start+event_end),event_end,time_end],[0.6,0.6,0.5,0.6,0.6]]
+        if simmode == 'size':
+            event_list[par_names.index('flxfin')] = [[time_start,event_start,0.5*(event_start+event_end),event_end,time_end],[0.5,0.5,0.1,0.5,0.5]]
+else: 
+    print '--- parameter values during event ---' 
+    print 'parameters you can change:\n'+' '.join(par_names)
+    print '-------------------------------------'
+    strlist = raw_input(
+        'Enter parameters which you want to change from the background values during events \n'
+        + 'choose from the above paramter list and put in single line (e.g.,  enter "d13c d18o capd47";\n'
+        + 'if you press enter without any input parameter values are not changed from their default values):\n\t'
+        )  
+    for par_name in strlist.split():
+        end_input = False
+        time_list = []
+        value_list = []
+        print 
+        while not end_input:
+            par_event_tmp = raw_input( 
+                'Specify time and '+par_name+' values'
+                + '(e.g., enter "1e3 1.5"):\n\t'
+                )
+            time_list.append(np.float64(par_event_tmp.split()[0]))
+            value_list.append(np.float64(par_event_tmp.split()[1]))
+            end_tmp = raw_input( 
+                'End to specify '+par_name +' [y/n]?: '
+                )
+            if end_tmp =='y': end_input = True
+        event_list[par_names.index(par_name)] = [time_list, value_list]
+
+par_values_spec = event_list
 
 for par_name in par_names:
     par_index = par_names.index(par_name)
@@ -116,12 +181,43 @@ np.savetxt(saveplace+'/'+filename,inputdata)
 #--- Specify recording time 
 n_rec = 15
 rectime = np.zeros(n_rec)
-rectime[:5]= np.linspace(dt,40e3,5)
-rectime[5:10]=np.linspace(40e3+dt,90e3,5)  # choose for diss. exp.
-rectime[10:15]=np.linspace(90e3+dt,200e3,5)  # choos for diss exp. 
+rectime[:5]= np.linspace(dt,event_start,5)
+rectime[5:10]=np.linspace(event_start+dt,event_end,5)  # choose for diss. exp.
+rectime[10:15]=np.linspace(event_end+dt,time_end,5)  # choos for diss exp. 
 # rectime[5:10]=np.linspace(40e3+dt,45e3,5)  # choose for biot exp
 # rectime[10:15]=np.linspace(45e3+dt,100e3,5)  # choose for biot exp
 
 
 filename = 'rectime.in'
 np.savetxt(saveplace+'/'+filename,rectime)
+
+if plt_timeline:
+    import matplotlib.pyplot as plt
+    
+    figsize = (10,5)
+    fig = plt.figure(figsize=figsize)
+    
+    nx =3 
+    ny =6 
+    
+    axes = [[plt.subplot2grid((ny,nx), (j,i)) for i in range(nx)] for j in range(ny)]
+    
+    for cnt in range(n_par):
+        i = cnt%nx
+        j = (cnt-i)/nx
+        
+        axes[j][i].plot(inputdata[:,0], inputdata[:,cnt])
+        axes[j][i].set_ylabel(par_names[cnt])
+        if j==ny-1:axes[j][i].set_xlabel(par_names[0]+' (yr)')
+        axes[j][i].axvline(x=event_start,ls=':',c='0.6')
+        axes[j][i].axvline(x=event_end,ls=':',c='0.6')
+        
+    fig.tight_layout()
+    
+    plt.savefig('imp_input.svg', transparent=True)
+    plt.savefig('imp_input.pdf', transparent=True)
+        
+    plt.show()
+    plt.clf()
+    plt.close()
+    
---
+++ input/readme_input.txt
@@ -0,0 +1,38 @@
+Readme on script/data in input directory
+
+/input/ directory has:
+
+1) labs-mtx.txt     : transition matrix created from a LABS simulation, used in all versions of codes [Fortran, Python, MATLAB]
+
+2) defines.h        : macros where you can modify options for Fortran version of codes
+
+3) make_input.py    : Python script used to create input files (reactime.in and imp_input.in) for all versions of codes [Fortran, Python, MATLAB]
+    
+    a) reactime.in  : listing model time at which simulated depth profiles are recorded
+    
+    b) imp_input.in : recording time changes of boundary conditions and input proxy signal values 
+    
+    The above two input files used for simulations in model development paper are stored in /input/EXAMPLES/ directory,
+        named as reactime_EXAMPLE-xxx.in and inp_input_EXAMPLE-xxx.in, respectively, and can be used by moving to /input/ directory
+        and re-naming them as rectime.in and imp_input.in respectively. 
+        E.g., in command line,             
+            'cd ../input' 
+            'cp EXAMPLES/rectime_EXAMPLE-xxx.in rectime.in'
+            'cp EXAMPLES/imp_input_EXAMPLE-xxx.in imp_input.in'
+    
+    You can run make_input.py by typing 'python make_input.py'. 
+    You are asked how you make input files: 
+        a) Use default backgroud values for input parameters 
+            a-1) if no, you are asked to choose the parameters you want to specify background values
+        b) Use default temporal changes of input parameter values 
+            b-1) if no, you are asked to enter the duration of simulation, and start and end time of signal/parameter change event
+                Then, you are asked to choose parameters you want to specigy the time evolution, and 
+                    to specigy time and proxy/parameter values at several points. 
+                    E.g., when you want to simulate a d18O spike (from 1 to -1 permil) between 20 and 30 kyr with total simulation duration 50 kyr, 
+                        you first choose 'd18O' and then enter '0 1', '20e3 1', '25e3 -1', '30e3 1', '50e3 1' line by line. 
+                        (You are instructed how to enter these values when you run the script.) 
+            b-2) if yes, asked the type of simulation you want ('biot', 'diss', 'size', 'iso', corresponding example simulations in model development paper).
+        c) Plot temporal changes of input parameter values
+            a-1) if yes, you will need module matplotlib. You can check immediately temporal changes of input parameters graphically. 
+                Plots are stored in /input/ directory in pdf and svg formats (imp_input.pdf and imp_input.svg). 
+    
